8d29b9e7bf87b111c9a6d0eedd2678123ca89dad
Nicolas Senave <nicolas.senave@gmail.com>
2023-08-07 15:32:01 +0200

    fix: update cors config after spring update

--- src/main/java/fr/insee/eno/ws/config/CorsConfig.java
+++ src/main/java/fr/insee/eno/ws/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setMaxAge(3600L); // disable for one hour OPTIONS request
