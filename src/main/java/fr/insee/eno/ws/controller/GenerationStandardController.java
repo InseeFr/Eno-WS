ad38fa81d2ac126331cb0fdeb6e6cbf2f31ac400
Nicolas Senave <nicolas.senave@gmail.com>
2024-01-08 19:01:19 +0100

    refactor: clean up

--- src/main/java/fr/insee/eno/ws/controller/GenerationStandardController.java
+++ src/main/java/fr/insee/eno/ws/controller/GenerationStandardController.java
@@ -1,39 +1,24 @@
 package fr.insee.eno.ws.controller;
 
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Files;
-
-import fr.insee.eno.ws.controller.utils.ResponseUtils;
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import fr.insee.eno.parameters.Capture;
-import fr.insee.eno.parameters.CaptureEnum;
-import fr.insee.eno.parameters.Context;
-import fr.insee.eno.parameters.ENOParameters;
-import fr.insee.eno.parameters.FOParameters;
-import fr.insee.eno.parameters.Format;
-import fr.insee.eno.parameters.Mode;
-import fr.insee.eno.parameters.OutFormat;
+import fr.insee.eno.parameters.*;
 import fr.insee.eno.service.ParameterizedGenerationService;
+import fr.insee.eno.ws.controller.utils.ResponseUtils;
 import fr.insee.eno.ws.service.ParameterService;
 import fr.insee.eno.ws.service.QuestionnaireGenerateService;
 import fr.insee.eno.ws.service.TransformService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.File;
+import java.io.InputStream;
 
 @Tag(name="Generation from DDI (standard parameters)")
 @RestController
@@ -64,12 +49,13 @@ public class GenerationStandardController {
 			)
 	@PostMapping(value="{context}/fo", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes= MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<StreamingResponseBody> generateFOQuestionnaire(
-
-			// Files
-			@RequestPart(value="in",required=true) MultipartFile in,
+			//
+			@RequestPart(value="in") MultipartFile in,
 			@RequestPart(value="specificTreatment",required=false) MultipartFile specificTreatment,
+			//
 			@RequestParam(value="Format-column",required=false) Integer nbColumn,
 			@RequestParam(value="Capture",required=false) CaptureEnum capture,
+			//
 			@PathVariable Context context) throws Exception {
 
 		LOGGER.info(
@@ -85,7 +71,8 @@ public class GenerationStandardController {
 	    if(capture!=null) {		
 	    	Capture capture2 = foParameters.getCapture();
 	    	capture2.setNumeric(capture);
-	    	foParameters.setCapture(capture2);;};
+	    	foParameters.setCapture(capture2);
+		}
 	    		
 	    if(nbColumn!=null) {
 	    Format format = foParameters.getFormat();
@@ -98,15 +85,11 @@ public class GenerationStandardController {
 		FileUtils.forceDelete(enoInput);
 
 		LOGGER.info("END of eno processing");
-		LOGGER.info("OutPut File :"+enoOutput.getName());
-
-		StreamingResponseBody stream = out -> out.write(Files.readAllBytes(enoOutput.toPath())) ;
+		LOGGER.info("OutPut File: {}", enoOutput.getName());
 		
 		return ResponseUtils.generateResponseFromFile(enoOutput);
 	}
-	
 
-	
 	@Operation(
 			summary="Generation of xforms questionnaire according to the context.",
 			description="It generates a xforms questionnaire from a ddi questionnaire using the default xforms parameters according to the study unit. "
@@ -114,11 +97,10 @@ public class GenerationStandardController {
 			)
 	@PostMapping(value="{context}/xforms", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes= MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<StreamingResponseBody> generateXformsQuestionnaire(
-
-			// Files
-			@RequestPart(value="in",required=true) MultipartFile in,
+			//
+			@RequestPart(value="in") MultipartFile in,
 			@RequestPart(value="specificTreatment",required=false) MultipartFile specificTreatment,
-
+			//
 			@PathVariable Context context) throws Exception {
 
 		LOGGER.info(
@@ -129,8 +111,7 @@ public class GenerationStandardController {
 		
 		return ResponseUtils.generateResponseFromFile(enoOutput);
 	}
-	
-	
+
 	@Operation(
 			summary="Generation of lunatic-xml questionnaire according  to the context.",
 			description="It generates a lunatic-xml questionnaire from a ddi questionnaire using the default js parameters according to the study unit. "
@@ -138,11 +119,10 @@ public class GenerationStandardController {
 			)
 	@PostMapping(value="{context}/lunatic-xml/{mode}", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes= MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<StreamingResponseBody> generateXMLLunaticQuestionnaire(
-
-			// Files
-			@RequestPart(value="in",required=true) MultipartFile in,
+			//
+			@RequestPart(value="in") MultipartFile in,
 			@RequestPart(value="specificTreatment",required=false) MultipartFile specificTreatment,
-
+			//
 			@PathVariable Context context,
 			@PathVariable Mode mode) throws Exception {
 
@@ -155,9 +135,7 @@ public class GenerationStandardController {
 		
 		return ResponseUtils.generateResponseFromFile(enoOutput);
 	}
-	
-	
-	
+
 	@Operation(
 			summary="Generation of pdf questionnaire according  to the context.",
 			description="It generates a lunatic-json-flat questionnaire from a ddi questionnaire using the default js parameters according to the study unit. "
@@ -165,13 +143,11 @@ public class GenerationStandardController {
 					+ "The params *parsingXpathVTL* must be 'true' (default value) if controls language is pseudo-xpath."
 			)
 	@PostMapping(value="{context}/lunatic-json/{mode}", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes= MediaType.MULTIPART_FORM_DATA_VALUE)
-	
 	public ResponseEntity<StreamingResponseBody> generateJSONLunaticQuestionnaire(
-			
-
-			@RequestPart(value="in",required=true) MultipartFile in,
+			//
+			@RequestPart(value="in") MultipartFile in,
 			@RequestPart(value="specificTreatment",required=false) MultipartFile specificTreatment,
-
+			//
 			@PathVariable Context context,
 			@PathVariable Mode mode) throws Exception {
 
@@ -193,13 +169,10 @@ public class GenerationStandardController {
 		FileUtils.forceDelete(enoInput);
 
 		LOGGER.info("END of eno processing");
-		LOGGER.info("OutPut File :"+enoOutput.getName());
-
-		StreamingResponseBody stream = out -> out.write(Files.readAllBytes(enoOutput.toPath())) ;
+		LOGGER.info("OutPut File: {}", enoOutput.getName());
 		
 		return ResponseUtils.generateResponseFromFile(enoOutput);
 	}
-	
 
 	@Operation(
 			summary="Generation of fodt questionnaire according  to the context.",
@@ -208,10 +181,9 @@ public class GenerationStandardController {
 			)
 	@PostMapping(value="{context}/fodt", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes= MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<StreamingResponseBody> generateFodtQuestionnaire(
-
-			// Files
-			@RequestPart(value="in",required=true) MultipartFile in,
-		
+			//
+			@RequestPart(value="in") MultipartFile in,
+			//
 			@PathVariable Context context) throws Exception {
 
 		LOGGER.info(
