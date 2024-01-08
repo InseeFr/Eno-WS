7ff33f5a382bc807299cf80af85195ea4db6e795
Nicolas Senave <nicolas.senave@gmail.com>
2024-01-08 19:21:14 +0100

    docs(controller): mark utils endpoints as deprecated

--- src/main/java/fr/insee/eno/ws/controller/UtilsController.java
+++ src/main/java/fr/insee/eno/ws/controller/UtilsController.java
@@ -38,9 +38,18 @@ public class UtilsController {
 		this.parser = parser;
     }
 
+	/**
+	 * Converts the given DDI in version 3.2 in a DDI in version 3.3.
+	 * @param in A DDI 3.2 file.
+	 * @return A DDI 3.3 file.
+	 * @throws Exception if conversion fails.
+	 * @deprecated DDI 3.2 is deprecated.
+	 */
     @Operation(summary = "Generation of ddi33 questionnaire from ddi32 questionnaire.",
 			description = "It generates a ddi in 3.3 version questionnaire from a a ddi in 3.2 version questionnaire.")
-	@PostMapping(value = "ddi32-2-ddi33", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@PostMapping(value = "ddi32-2-ddi33",
+			produces = MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@Deprecated(since = "2.0.0")
 	public ResponseEntity<StreamingResponseBody> generateDDI33Questionnaire(
 			@RequestPart(value = "in") MultipartFile in) throws Exception {
 
@@ -68,9 +77,16 @@ public class UtilsController {
 				.body(stream);
 	}
 
+	/**
+	 * Converts a Xpath 1.1 expression into a VTL 2.0 expression
+	 * @param xpath A string XPath 1.1 expression
+	 * @return A string VTL 2.0 expression
+	 * @deprecated This endpoint and its logic class has been copied/pasted in the Eno Java web-service.
+	 */
 	@Operation(summary = "Generation of VTL formula from Xpath formula",
 			description = "It generates a VTL in 2.0 version from a Xpath in 1.1 version.")
 	@PostMapping(value = "xpath-2-vtl")
+	@Deprecated(since = "2.0.0")
 	public ResponseEntity<String> generateVTLFormula(
 			@RequestParam(value = "xpath") String xpath) {
 
@@ -79,10 +95,19 @@ public class UtilsController {
 		return ResponseEntity.ok()
 				.body(vtl);
 	}
-	
+
+	/**
+	 * Converts the given Lunatic XML questionnaire file into a Lunatic JSON questionnaire file.
+	 * @param in A Lunatic XML questionnaire file.
+	 * @return A Lunatic JSON questionnaire file.
+	 * @throws Exception if conversion fails.
+	 * @deprecated The Lunatic XML format is deprecated.
+	 */
 	@Operation(summary = "Generation of Lunatic Json from Lunatic XML",
 			description = "It generates a Lunatic Json from a Lunatic XML, using the Lunatic-Model library.")
-	@PostMapping(value = "lunatic-model/xml-2-json", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@PostMapping(value = "lunatic-model/xml-2-json",
+			produces = MediaType.APPLICATION_OCTET_STREAM_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@Deprecated(since = "2.0.0")
 	public ResponseEntity<StreamingResponseBody> generateLunaticJson(
 			@RequestPart(value = "in") MultipartFile in) throws Exception {
 		
