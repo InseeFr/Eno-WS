6d123da9b1158fcb8eb376edaa81c7f9be9696a8
Nicolas Senave <nicolas.senave@gmail.com>
2024-01-09 13:38:49 +0100

    style: tiny refactor

--- src/main/java/fr/insee/eno/ws/config/OpenApiConfiguration.java
+++ src/main/java/fr/insee/eno/ws/config/OpenApiConfiguration.java
@@ -33,8 +33,8 @@ public class OpenApiConfiguration {
 	@Bean
 	public OpenAPI customOpenAPI() {
 		Server server = new Server();
-		server.setUrl(apiScheme+"://"+apiHost);		
-		OpenAPI openAPI = new OpenAPI()
+		server.setUrl(apiScheme+"://"+apiHost);
+		return new OpenAPI()
 				.addServersItem(server)
 				.info(
 						new Info()
@@ -43,12 +43,11 @@ public class OpenApiConfiguration {
 								"<h2>Generator using :</h2>"
 							  + "<style>.cell{border: black 2px solid; text-align: center; font-weight: bold; font-size: 1.5em;} .version{color:darkred}</style>"
 							  + "<table style=\"width:40%\">"
-							  + "	<tr><td class=\"cell\">Eno version</td><td class=\"cell version\">"+enoVersion+"</td></tr>"
-							  + "	<tr><td class=\"cell\">Lunatic Model version</td><td class=\"cell version\">"+lunaticModelVersion+"</td></tr>"
+							  + "<tr><td class=\"cell\">Eno version</td><td class=\"cell version\">"+enoVersion+"</td></tr>"
+							  + "<tr><td class=\"cell\">Lunatic Model version</td><td class=\"cell version\">"+lunaticModelVersion+"</td></tr>"
 							  + "</table>")
 						.version(projectVersion)
 						.license(new License().name("Apache 2.0").url("http://springdoc.org"))
 						);
-		return openAPI;
 	}
 }
