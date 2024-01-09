c5b20f008c5c20c7afa6e296fccb7cf8e8248c18
Nicolas Senave <nicolas.senave@gmail.com>
2024-01-09 10:09:29 +0100

    refactor: add logging and clean up

--- src/main/java/fr/insee/eno/ws/controller/ParametersController.java
+++ src/main/java/fr/insee/eno/ws/controller/ParametersController.java
@@ -1,30 +1,23 @@
 package fr.insee.eno.ws.controller;
 
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Files;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import fr.insee.eno.parameters.OutFormat;
 import fr.insee.eno.parameters.Context;
 import fr.insee.eno.parameters.Mode;
+import fr.insee.eno.parameters.OutFormat;
 import fr.insee.eno.ws.service.ParameterService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 @Tag(name="Parameters")
 @RestController
@@ -45,6 +38,8 @@ public class ParametersController {
 	@GetMapping(value="all", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE)
 	public ResponseEntity<StreamingResponseBody> getDefaultParam() throws Exception {
 
+		LOGGER.info("Get request for parameters file with all params.");
+
 		InputStream paramsInputStream = parameterService.getDefaultParametersIS();
 
 		StreamingResponseBody stream = out -> out.write(IOUtils.toByteArray(paramsInputStream));
@@ -62,31 +57,19 @@ public class ParametersController {
 			@PathVariable Context context,
 			@PathVariable OutFormat outFormat,
 			@RequestParam(value="Mode",required=false) Mode mode) throws Exception {
-		File fileParam;
 
-		switch (outFormat) {
-		case XFORMS:
-			fileParam=parameterService.getDefaultCustomParametersFile(context, OutFormat.XFORMS,mode);
-			break;
-		case FO:
-			fileParam=parameterService.getDefaultCustomParametersFile(context, OutFormat.FO,mode);
-			break;
-		case LUNATIC_XML:
-			fileParam=parameterService.getDefaultCustomParametersFile(context, OutFormat.LUNATIC_XML,mode);
-			break;
-		case DDI:
-			fileParam=parameterService.getDefaultCustomParametersFile(Context.DEFAULT, OutFormat.DDI,mode);
-			break;
-		case FODT:
-			fileParam=parameterService.getDefaultCustomParametersFile(context, OutFormat.FODT,mode);
-			break;
-		default:
-			fileParam = File.createTempFile("default-param-"+ (outFormat==OutFormat.DDI?"default" :context.value().toLowerCase()) +"-"+ (outFormat==OutFormat.LUNATIC_XML?mode.value().toLowerCase():""), ".xml");
-			
-			FileUtils.copyInputStreamToFile(parameterService.getDefaultParametersIS(), fileParam);
-			break;
-		}
-		StreamingResponseBody stream = out -> out.write(Files.readAllBytes(fileParam.toPath()));
+		LOGGER.info("Get request for parameters file with context {}, mode {}, out format {}.",
+				context, mode, outFormat);
+
+		File fileParam = switch (outFormat) {
+            case XFORMS -> parameterService.getDefaultCustomParametersFile(context, OutFormat.XFORMS, mode);
+            case FO -> parameterService.getDefaultCustomParametersFile(context, OutFormat.FO, mode);
+            case LUNATIC_XML -> parameterService.getDefaultCustomParametersFile(context, OutFormat.LUNATIC_XML, mode);
+            case DDI -> parameterService.getDefaultCustomParametersFile(Context.DEFAULT, OutFormat.DDI, mode);
+            case FODT -> parameterService.getDefaultCustomParametersFile(context, OutFormat.FODT, mode);
+        };
+
+        StreamingResponseBody stream = out -> out.write(Files.readAllBytes(fileParam.toPath()));
 
 		return  ResponseEntity.ok()
 				.header(HttpHeaders.CONTENT_DISPOSITION,
